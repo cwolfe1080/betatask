36265d6511c96dca8d7c6a8a13c64ac187dbc04d
cwolfe1080 <cewolfe19@gmail.com>
2026-04-01 15:15:33 -0400

    Initial commit: add bash script

---
+++ main.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+clear
+
+echo "BetaTask V0.1"
+echo ""
+while true; do
+	echo -n ">>> "
+
+	read -r input
+	if [ "$input" == "help" ]; then
+		echo "Commands:"
+		echo "help - Open this help menu"
+		echo "exit - Quit BetaTask"
+		echo "info - See BetaTask Version Info"
+		echo "temp - Get CPU temp"
+		echo "clear - Clear the terminal"
+	elif [ "$input" == "exit" ]; then
+		clear
+		exit
+	elif [ "$input" == "info" ]; then
+		echo "BetaTask by cwolfe1080"
+		echo "Version V0.1"
+		echo "Version date: 04/01/26 MM/DD/YYYY"
+	elif [ "$input" == "temp" ]; then
+		echo "Temp is not set up yet"
+	elif [ "$input" == "clear" ]; then
+		clear
+	
+
+	else
+		echo "Command ''$input' was not recognized"	
+	fi
+done
